104a700a0b899e7db7182c4c7d02065e2c7cceae
ddyer0 <ddyer-bitbucket@real-me.net>
2023-12-09 18:58:12 -0800

    new installers for windows

--- client/boardspace-java/deploy-and-sign/pc-signing/jbuild.bat
+++ client/boardspace-java/deploy-and-sign/pc-signing/jbuild.bat
@@ -2,9 +2,10 @@ rem
 rem this builds an installer using the official jpackage method
 rem the magic number for sha1 has to be the fingerprint of the current signing certificate
 rem
-
-copy g:\share\projects\boardspace-html\htdocs\java\v102\boardspace.jar boardspace.jar
-F:/java/jdk-16/bin/jpackage.exe --win-shortcut --win-menu -i boardspace --main-jar "boardspace.jar" -t "msi" --app-version 1.0 --copyright "Dave Dyer" --description "Desktop launcher for Boardspace.net" --icon boardspace_icon_128-indexed.ico
+mkdir temp
+copy g:\share\projects\boardspace-html\htdocs\java\v102\boardspace.jar temp\boardspace.jar
+F:/java/jdk-16/bin/jpackage.exe  --win-shortcut --win-menu -i "g:/share/projects/boardspace-java/deploy-and-sign/pc-signing/temp/" --main-jar "boardspace.jar" -t "msi" --app-version 1.0 --copyright "Dave Dyer" --description "Desktop launcher for Boardspace.net" --icon boardspace_icon_128-indexed.ico --name "Boardspace"
+rm -rf temp
 copy Boardspace-1.0.msi unsigned\
 del Boardspace-1.0.msi
 copy unsigned\Boardspace-1.0.msi signed\Boardspace-1.0.msi
